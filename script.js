0ebe27b73f7e900a62706cce39bf2a50ab513f63
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-02-07 00:09:52 +1000

    Add  function playRound to script.js file

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function computerPlay(min, max) {
-let result = Math.floor(Math.random() * (max-min) ) + min;
-if (result === 1) {
+  let result = Math.floor(Math.random() * (max-min) ) + min;
+    if (result === 1) {
     return "Rock"
-} else if(result === 2) {
-    return "Paper"
-} else {
-    return "Scissors"
+    } else if(result === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
- }
-console.log(computerPlay(1, 4))
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection =="Rock" && computerSelection =="Scissors" ||
+      playerSelection =="Paper" && computerSelection =="Rock" ||
+      playerSelection =="Scissors" && computerSelection =="Paper") {
+       return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+  else if (playerSelection =="Scissors" && computerSelection =="Rock" ||
+           playerSelection =="Rock" && computerSelection =="Paper" ||
+           playerSelection =="Paper" && computerSelection =="Scissors") {
+      return "You Lose! " + computerSelection + " beats " + playerSelection;
+    } 
+      else {
+        return "Draw! Play Again"
+      }
+}
