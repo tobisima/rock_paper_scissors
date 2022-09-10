c8d86d98871d35cac746ccf00f19611b3b892a77
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-09-10 22:51:20 +1000

    Split a long string into two parts

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 function computerPlay() {
- const result = Math.floor(Math.random() * 3);
+ 
+   const result = Math.floor(Math.random() * 3);
 
-    if (result === 1) {
+     if (result === 1) {
         return "Rock"
-    } else if(result === 2) {
+     } else if(result === 2) {
         return "Paper"
-    } else {
+     } else {
         return "Scissors"
-    }
+     }
 }
 
 function playRound(playerSelection, computerSelection) {
-   const playerSelectionI = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+   const playerSelectionI = playerSelection.charAt(0).toUpperCase() +
+         playerSelection.toLowerCase().slice(1);
   
-   console.log(playerSelectionI, computerSelection);
+         console.log(playerSelectionI, computerSelection);
   
-  if (playerSelectionI =="Rock" && computerSelection =="Scissors" ||
-      playerSelectionI =="Paper" && computerSelection =="Rock" ||
-      playerSelectionI =="Scissors" && computerSelection =="Paper") {
-      return "You Win! " + playerSelectionI + " beats " + computerSelection;
-  } else if (playerSelectionI == computerSelection) {
+     if (playerSelectionI =="Rock" && computerSelection =="Scissors" ||
+         playerSelectionI =="Paper" && computerSelection =="Rock" ||
+         playerSelectionI =="Scissors" && computerSelection =="Paper") {
+     return "You Win! " + playerSelectionI + " beats " + computerSelection;
+     } else if (playerSelectionI == computerSelection) {
       return "Draw!"
-  } else {
+     } else {
       return "You Lose! " + computerSelection + " beats " + playerSelectionI;
   }
 }
 
 function game(playerScore, computerScore) {
   for (let i = 0; i < 5; i++) {
-     const playRoundResult = playRound( prompt("Enter 'Rock' or 'Paper' or 'Scissors'"), computerPlay() );
+    const playRoundResult = playRound(
+        prompt("Enter 'Rock' or 'Paper' or 'Scissors'"), computerPlay() );
     
-      if (playRoundResult.charAt(4) == "W") {
+    if (playRoundResult.charAt(4) == "W") {
         ++playerScore;
-      } else if (playRoundResult.charAt(4) == "L") {
+    } else if (playRoundResult.charAt(4) == "L") {
         ++computerScore;
-      } else {}
+    } else {}
 
         console.log(playRoundResult); 
         console.log("score: " + playerScore, computerScore);
   }
   
-      if (playerScore > computerScore) {
+    if (playerScore > computerScore) {
           return "Game Over! You Win!"
-      } else if (playerScore < computerScore) {
+    } else if (playerScore < computerScore) {
           return "Game Over! You Lose!"
       } else {
           return "Game Over! Draw!"
