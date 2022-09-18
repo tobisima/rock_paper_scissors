87319cd2135070131d32a18056d3c2d08486f8bc
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-09-18 22:15:52 +1000

    Change all "if" conditionals to "?" conditionals

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
 function computerPlay() {
- 
-   const result = Math.floor(Math.random() * 3);
 
-     if (result === 1) {
-        return "Rock"
-     } else if(result === 2) {
-        return "Paper"
-     } else {
-        return "Scissors"
-     }
+   const randomNumber = Math.floor(Math.random() * 3);
+
+  return randomNumber === 1 ? "Rock" :
+         randomNumber === 2 ? "Paper" : "Scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+
          playerSelection = playerSelection.charAt(0).toUpperCase() +
          playerSelection.toLowerCase().slice(1);
-  
+
          console.log(playerSelection, computerSelection);
-  
-     if (playerSelection =="Rock" && computerSelection =="Scissors" ||
-         playerSelection =="Paper" && computerSelection =="Rock" ||
-         playerSelection =="Scissors" && computerSelection =="Paper") {
-     return "You Win! " + playerSelection + " beats " + computerSelection;
-     } else if (playerSelection == computerSelection) {
-      return "Draw!"
-     } else {
-      return "You Lose! " + computerSelection + " beats " + playerSelection;
-  }
+
+  return playerSelection == computerSelection ? "Draw!" :
+         playerSelection == "Rock" && computerSelection == "Scissors" ||
+         playerSelection == "Paper" && computerSelection == "Rock" ||
+         playerSelection == "Scissors" && computerSelection == "Paper" ?
+         "You Win! " + playerSelection + " beats " + computerSelection :
+         "You Lose! " + computerSelection + " beats " + playerSelection;
 }
 
 function game(playerScore, computerScore) {
-  for (let i = 0; i < 5; i++) {
-    const roundResult = playRound(
-        prompt("Enter 'Rock' or 'Paper' or 'Scissors'"), computerPlay());
-    
-    if (roundResult.charAt(4) == "W") {
-        ++playerScore;
-    } else if (roundResult.charAt(4) == "L") {
-        ++computerScore;
-    }    
 
-        console.log(roundResult); 
-        console.log("score: " + playerScore, computerScore);
-  }
-  
-    if (playerScore > computerScore) {
-          return "Game Over! You Win!"
-    } else if (playerScore < computerScore) {
-          return "Game Over! You Lose!"
-      } else {
-          return "Game Over! Draw!"
+    for (let i = 0; i < 5; i++) {
+
+         const roundResult = playRound(prompt(
+               "Enter 'Rock' or 'Paper' or 'Scissors'"), computerPlay());
+    
+         roundResult.charAt(4) == "W" ?
+         ++playerScore :
+         roundResult.charAt(4) == "L" ?
+         ++computerScore : playerScore += 0;
+        
+         console.log(roundResult); 
+         console.log("score: " + playerScore, computerScore);
     }
+  
+  return playerScore > computerScore ?
+         "Game Over! You Win!" :
+         playerScore < computerScore ?
+         "Game Over! You Lose!" :
+         "Game Over! Draw!";
 }
   
 console.log(game(0, 0));
