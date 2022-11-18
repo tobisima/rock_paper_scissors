c3bf31de30b136a7647eb6348cba1749bfc2f82b
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2022-11-18 19:40:59 +1000

    Make UI

--- script.js
+++ script.js
@@ -1,47 +1,79 @@
+const body = document.querySelector('body');
+const labels = document.querySelectorAll('.labels');
+const playerLabel = document.querySelector('#player-label');
+const computerLabel = document.querySelector('#computer-label');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+const fieldFooter = document.querySelector('.field-footer');
+const playAgainButton = document.createElement('button');
+const winnerAnnouncement = document.createElement('h3')
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons ) {
+  button.onclick = () => playRound(button.textContent, computerPlay());
+}
+
+labels.forEach(label => label.addEventListener('transitionend', removeTransition));
+
+function removeTransition() {
+labels.forEach(label => label.classList.remove('zoom-by-click'));
+}
+
 function computerPlay() {
-
-   const randomNumber = Math.floor(Math.random() * 3);
-
+  const randomNumber = Math.floor(Math.random() * 3);
   return randomNumber === 1 ? "Rock" :
-         randomNumber === 2 ? "Paper" : "Scissors";
+  randomNumber === 2 ? "Paper" : "Scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-
-         playerSelection = playerSelection.charAt(0).toUpperCase() +
-                           playerSelection.toLowerCase().slice(1);
-
-         console.log(playerSelection, computerSelection);
-
-  return playerSelection == computerSelection ? "Draw!" :
-         playerSelection == "Rock" && computerSelection == "Scissors" ||
-         playerSelection == "Paper" && computerSelection == "Rock" ||
-         playerSelection == "Scissors" && computerSelection == "Paper" ?
-         "You Win! " + playerSelection + " beats " + computerSelection :
-         "You Lose! " + computerSelection + " beats " + playerSelection;
-}
-
-function game(playerScore, computerScore) {
-
-    for (let i = 0; i < 5; i++) {
-
-         const roundResult = playRound(prompt(
-               "Enter 'Rock' or 'Paper' or 'Scissors'"),computerPlay());
-    
-         roundResult.charAt(4) == "W" ?
-         ++playerScore :
-         roundResult.charAt(4) == "L" ?
-         ++computerScore : playerScore += 0;
-        
-         console.log(roundResult); 
-         console.log("score: " + playerScore, computerScore);
-    }
-  
-  return playerScore > computerScore ?
-         "Game Over! You Win!" :
-         playerScore < computerScore ?
-         "Game Over! You Lose!" :
-         "Game Over! Draw!";
+  playerLabel.textContent = playerSelection; 
+  computerLabel.textContent = computerSelection;
+  if (playerSelection == "Rock" && computerSelection == "Scissors" ||
+    playerSelection == "Paper" && computerSelection == "Rock" ||
+    playerSelection == "Scissors" && computerSelection == "Paper") {
+    fieldFooter.textContent =
+    "You Win! " + playerSelection + " beats " + computerSelection;
+    playerScore.textContent++;
+  } else if (playerSelection == computerSelection) {
+    fieldFooter.textContent = "Draw!";
+  } else { 
+    fieldFooter.textContent =
+    "You Lose! " + computerSelection + " beats " + playerSelection;
+    computerScore.textContent++;
+  }  
+  announceWinner();
+  labels.forEach(label => label.classList.add('zoom-by-click'));
 }
   
-console.log(game(0, 0));
+function announceWinner() {
+  if (playerScore.textContent == 5) {
+    body.appendChild(winnerAnnouncement);
+    winnerAnnouncement.textContent = 'Game Over! You Won!';  
+    offerNewGame();
+  } else if (computerScore.textContent == 5) {
+    body.appendChild(winnerAnnouncement);
+    winnerAnnouncement.textContent = 'Game Over! Computer Won!'; 
+    offerNewGame();
+  }
+}
+
+function offerNewGame() {
+  buttons.forEach(button => body.removeChild(button));
+  playAgainButton.classList.add('play-again');
+  playAgainButton.textContent = 'Play Again';
+  body.appendChild(playAgainButton);
+}
+
+function startNewGame() {
+  body.removeChild(playAgainButton);
+  body.removeChild(winnerAnnouncement);          
+  buttons.forEach(button => body.appendChild(button));
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  playerLabel.textContent = 'Rock';
+  computerLabel.textContent = 'Rock';
+  fieldFooter.textContent = 'Click Rock, Paper or Scissors';
+}
+  
+  playAgainButton.onclick = startNewGame;
+  
